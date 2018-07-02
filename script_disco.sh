9d75840ef0867c45a58b01c57960e1f58c019b54
cl4udino <lhcs10@gmail.com>
2018-07-01 21:31:14 -0300

    Add files via upload
    Scripts de execução

---
+++ script_disco.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance1.xls
+echo "Concluido 1"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance2.xls
+echo "Concluido 2"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance3.xls
+echo "Concluido 3"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance4.xls
+echo "Concluido 4"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance5.xls
+echo "Concluido 5"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance6.xls
+echo "Concluido 6"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance7.xls
+echo "Concluido 7"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance8.xls
+echo "Concluido 8"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance9.xls
+echo "Concluido 9"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance10.xls
+echo "Concluido 10"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance11.xls
+echo "Concluido 11"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance12.xls
+echo "Concluido 12"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance13.xls
+echo "Concluido 13"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance14.xls
+echo "Concluido 14"
+iozone -a -i 0 -i 1 -s 64 -s 128 -s 256 -s 512 -s 1024 -s 2048 -s 4096 -s 8192 -b performance15.xls
+echo "Concluido 15"
+
---
+++ script_disco2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa1.xls
+echo "Concluido 1"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa2.xls
+echo "Concluido 2"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa3.xls
+echo "Concluido 3"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa4.xls
+echo "Concluido 4"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa5.xls
+echo "Concluido 5"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa6.xls
+echo "Concluido 6"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa7.xls
+echo "Concluido 7"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa8.xls
+echo "Concluido 8"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa9.xls
+echo "Concluido 9"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa10.xls
+echo "Concluido 10"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa11.xls
+echo "Concluido 11"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa12.xls
+echo "Concluido 12"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa13.xls
+echo "Concluido 13"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa14.xls
+echo "Concluido 14"
+iozone -t 4 -t 8 -t 16 -t 32 -t 64 -i 0 -i 1 -s 10024 -b taxa15.xls
+echo "Concluido 15"
